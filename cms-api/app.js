8163b37842e4d752a1737db2458638b2a73a08e1
bakti <bakti.widianto@gmail.com>
2020-07-20 21:07:39 +0700

    modifed

--- cms-api/app.js
+++ cms-api/app.js
@@ -10,6 +10,7 @@ console.log(`You're connected to server!`)
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var dataRouter = require('./routes/data');
 
 var app = express();
 
@@ -21,5 +22,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/data', dataRouter);
 
 module.exports = app;
