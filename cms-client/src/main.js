3e49294f112447464ec1bb5a924673e97720b11f
bakti <bakti.widianto@gmail.com>
2020-07-23 04:09:50 +0700

    modifed to add component

--- cms-client/src/main.js
+++ cms-client/src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router';
+import axios from 'axios';
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
+
+
+// assign prototype axios to each vue instance
+Vue.prototype.$axios = axios;
