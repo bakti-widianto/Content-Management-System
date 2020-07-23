ebb1c7a9552e1b0bd715dccd629520782c374359
bakti <bakti.widianto@gmail.com>
2020-07-23 17:20:42 +0700

    CRUD datedata done

---
+++ cms-client/src/components/Datedata.vue
@@ -0,0 +1,304 @@
+<template>
+  <div>
+    <navbar></navbar>
+    <h1 class="text-center mt-2">Data Date Dashboard</h1>
+    <hr />
+    <!-- ADD -->
+    <div class="container">
+      <div class="collapse" id="form-add">
+        <div>
+          <form>
+            <div class="row">
+              <div class="col-2">
+                <h3>
+                  <i class="far fa-plus-square">Add :</i>
+                </h3>
+              </div>
+              <div class="col-4">
+                <input type="date" v-model="letter" class="form-control" required />
+              </div>
+              <div class="col-4">
+                <input
+                  type="text"
+                  v-model="frequency"
+                  class="form-control"
+                  placeholder="Frequency"
+                  required
+                />
+              </div>
+              <button
+                class="btn btn-secondary"
+                data-toggle="collapse"
+                data-target="#form-add"
+                @click="hendleAdd"
+              >save</button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <br />
+      <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#form-add">
+        <span>
+          <i class="fas fa-plus"></i> add
+        </span>
+      </button>
+    </div>
+    <!-- END ADD -->
+    <!-- EDIT -->
+    <br />
+    <div class="container">
+      <div class="collapse" id="form-edit">
+        <div>
+          <form>
+            <div class="row">
+              <div class="col-2">
+                <h3>
+                  <i class="fas fa-edit">Edit :</i>
+                </h3>
+              </div>
+              <div class="col-4">
+                <input type="date" v-model="eLetter" class="form-control" placeholder="Letter" />
+              </div>
+              <div class="col-4">
+                <input
+                  type="text"
+                  v-model="eFrequency"
+                  class="form-control"
+                  placeholder="Frequency"
+                />
+              </div>
+              <button
+                class="btn btn-secondary"
+                data-toggle="collapse"
+                data-target="#form-edit"
+                @click="handleUpdate"
+              >save</button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <br />
+    </div>
+    <!-- END EDIT -->
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class="card">
+            <div class="card-header text-white bg-dark">
+              <br />
+              <form>
+                <div class="row">
+                  <div class="col-2">
+                    <h3>
+                      <i class="fas fa-search">Search</i>
+                    </h3>
+                  </div>
+                  <div class="col-4">
+                    <input
+                      type="text"
+                      v-model="sLetter"
+                      class="form-control"
+                      placeholder="Latter"
+                      required
+                    />
+                  </div>
+                  <div class="col-4">
+                    <input
+                      type="Number"
+                      step="0.1"
+                      v-model="sFrequency"
+                      class="form-control"
+                      placeholder="Frequency"
+                      required
+                    />
+                  </div>
+                </div>
+              </form>
+            </div>
+            <div class="card-body">
+              <table class="table table-striped text-center">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Letter</th>
+                    <th scope="col">Frequency</th>
+                    <th scope="col">Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for=" (data,index) in datas" v-bind:key="data._id">
+                    <th>{{index + 1}}</th>
+                    <td>{{data.letter}}</td>
+                    <td>{{data.frequency}}</td>
+                    <td>
+                      <button
+                        role="button"
+                        v-bind:id="data._id"
+                        data-toggle="collapse"
+                        data-target="#form-edit"
+                        class="btn btn-success"
+                        @click="getUpdate"
+                      >
+                        <i class="fas fa-edit">update</i>
+                      </button>
+
+                      <button
+                        role="button"
+                        class="btn btn-danger"
+                        v-bind:id="data._id"
+                        onclick="return confirm('Are you sure you want to delete this item?');"
+                        @click="handleDelete"
+                      >
+                        <i class="fas fa-trash-alt">delete</i>
+                      </button>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from "./Navbar";
+
+export default {
+  components: {
+    navbar: Navbar,
+  },
+  data() {
+    return {
+      datas: null,
+      letter: "",
+      frequency: "",
+      eLetter: "",
+      eFrequency: "",
+      sLetter: "",
+      sFrequency: "",
+      id: "",
+    };
+  },
+  watch: {
+    sLetter: function () {
+      this.searchData();
+      //   console.log(this.sLetter);
+    },
+    sFrequency: function () {
+      this.searchData();
+      //   console.log(this.sFrequency);
+    },
+  },
+  methods: {
+    loadData() {
+      this.$axios
+        .get("http://localhost:3000/api/datadate/")
+        .then((response) => {
+          //   console.log(response.data);
+          this.datas = response.data;
+        })
+        .catch((err) => console.log(err));
+    },
+    hendleAdd(e) {
+      e.preventDefault();
+      //   console.log(this.letter, this.frequency)
+      this.$axios
+        .post("http://localhost:3000/api/datadate/", {
+          letter: this.letter,
+          frequency: this.frequency,
+        })
+        .then((response) => {
+          if (response.data.success == true) {
+            this.letter = "";
+            this.frequency = "";
+            this.loadData();
+          } else {
+            console.log("internal server error to Add");
+          }
+        })
+        .catch((err) => console.log("Something wrong with API connection"));
+    },
+    getUpdate(e) {
+      e.preventDefault();
+      const id = event.currentTarget.id;
+      this.$axios
+        .get("http://localhost:3000/api/datadate/" + id)
+        .then((result) => {
+          if (result.data.success == true) {
+            // console.log(result.data);
+            this.eLetter = result.data.data.letter;
+            this.eFrequency = result.data.data.frequency;
+            this.id = result.data.data._id;
+          } else {
+            console.log("error bang");
+          }
+        })
+        .catch((err) => console.log(err));
+    },
+    handleUpdate(e) {
+      e.preventDefault();
+      this.$axios
+        .put("http://localhost:3000/api/datadate/" + this.id, {
+          letter: this.eLetter,
+          frequency: this.eFrequency,
+        })
+        .then((response) => {
+          if (response.data.success == true) {
+            this.eLetter = "";
+            this.eFrequency = "";
+            this.id = "";
+            this.loadData();
+          } else {
+            console.log("error update");
+          }
+        })
+        .catch((err) => console.log(err));
+    },
+    handleDelete(e) {
+      e.preventDefault();
+      const id = event.currentTarget.id;
+      console.log(id);
+      this.$axios
+        .delete("http://localhost:3000/api/datadate/" + id)
+        .then((response) => {
+          if (response.data.success == true) {
+            this.loadData();
+          } else console.log("Gagal remove");
+        })
+        .catch((err) => console.log(err));
+    },
+    searchData() {
+      let search = {};
+      if (this.sLetter && this.sFrequency) {
+        search.letter = this.sLetter;
+        search.frequency = this.sFrequency;
+      } else if (this.sLetter) {
+        search.letter = this.sLetter;
+      } else if (this.sFrequency) {
+        search.frequency = this.sFrequency;
+      }
+      this.$axios
+        .post("http://localhost:3000/api/datadate/search", search)
+        .then((response) => {
+          this.datas = response.data;
+        })
+        .catch((err) => console.log(err));
+    },
+  },
+  mounted() {
+    this.loadData();
+  },
+};
+</script>
+
+<style>
+.card {
+  border: 0;
+  border-radius: 10px;
+  box-shadow: 0 0.5rem 0.5rem 0 rgba(0, 0, 0, 0.3);
+  overflow: hidden;
+}
+</style>
