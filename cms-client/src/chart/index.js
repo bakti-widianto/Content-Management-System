51d3882b407d8bb21e3e9a295764b1c80b12f6a7
bakti <bakti.widianto@gmail.com>
2020-07-23 17:22:07 +0700

    add pie chart

--- cms-client/src/chart/index.js
+++ cms-client/src/chart/index.js
@@ -24,4 +24,28 @@ function barChart(apiData) {
    }
 }
 
-export { barChart };
+//Pie Chart
+function pieChart(apiData) {
+   google.charts.load("current", { packages: ["corechart"] });
+   google.charts.setOnLoadCallback(drawChart);
+   function drawChart() {
+      let rawData = apiData.map(item => [item.letter, item.frequency])
+      rawData.unshift(['Subject', 'Frequency'])
+      // console.log(rawData)
+
+      var data = google.visualization.arrayToDataTable(rawData);
+
+      var options = {
+         title: 'Frequency for each subject',
+         is3D: true,
+         width: 800,
+         height: 500
+      };
+
+      var chart = new google.visualization.PieChart(document.getElementById('pie-chart'));
+      chart.draw(data, options);
+   }
+
+}
+
+export { barChart, pieChart };
