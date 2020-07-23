3f8b6b24eba16477b8494d629c9a64a338f3c988
bakti <bakti.widianto@gmail.com>
2020-07-24 05:17:42 +0700

    add maps chart

--- cms-client/src/chart/index.js
+++ cms-client/src/chart/index.js
@@ -48,7 +48,7 @@ function pieChart(apiData) {
 
 }
 
-//Pie Chart
+//Line Chart
 function lineChart(dataDates) {
    google.charts.load('current', { 'packages': ['line'] });
    google.charts.setOnLoadCallback(drawChart);
@@ -64,7 +64,7 @@ function lineChart(dataDates) {
       var options = {
          title: "Frequency Rate for Each Date",
          width: "100%",
-         height: 400,
+         height: 500,
          hAxis: { title: "Date" },
          vAxis: { title: "Frequency" },
          legend: { position: "none" },
@@ -72,10 +72,37 @@ function lineChart(dataDates) {
       };
 
       var chart = new google.charts.Line(document.getElementById('linechart_material'));
-
       chart.draw(data, google.charts.Line.convertOptions(options));
    }
 }
 
+//Map Chart
+function mapChart(apiData) {
+   google.charts.load('current', {
+      'packages': ['map'],
+      'mapsApiKey': 'AIzaSyBvu5r9W_wbQcAn9e5qYfMX1_Eo7t-2qmI'
 
-export { barChart, pieChart, lineChart };
+   });
+   google.charts.setOnLoadCallback(drawMap);
+
+   function drawMap() {
+      let rawData = apiData.map(item => [item.lat, item.lng, item.title]);
+      rawData.unshift(['Lat', 'Long', 'Name'])
+      var data = google.visualization.arrayToDataTable(rawData);
+
+      var options = {
+         showTooltip: true,
+         showInfoWindow: true,
+         zoomLevel: 13,
+         width: "100%",
+         height: 500,
+      };
+
+      var map = new google.visualization.Map(document.getElementById('chart_div'));
+
+      map.draw(data, options);
+   };
+}
+
+
+export { barChart, pieChart, lineChart, mapChart };
